8106378e2b15c1d991212c9b4ab00f2a9aa1cc2a
Oly <Oly@yamums.house>
2025-04-12 19:30:30 -0300

    fix formatting on install-win.ps1

--- script/install-win.ps1
+++ script/install-win.ps1
@@ -3,12 +3,16 @@ $binPath = "$env:USERPROFILE\.local\bin"
 $exePath = Join-Path $binPath "kt-encabulator.exe"
 
 if (Test-Path -Path $exePath) {
-    Write-Host "`n> Found existing binary in local bin. Do you wish to Continue? `e[1;34m(y/n)`e[0m"
+    Write-Host -NoNewline "`n> Found existing binary in local bin. Do you wish to Continue? "
+    Write-Host "(y/n)" -ForegroundColor Blue
+
     $choice = Read-Host
     if ($choice -match '^[yY]$') {
         Remove-Item -Path $exePath -Force
-    } else {
-        Write-Host "The existing file will not be overwritten. `e[1;34mExiting...`e[0m" -ForegroundColor Blue
+    }
+
+    else {
+        Write-Host "The existing file will not be overwritten. Exiting..." -ForegroundColor Blue
         exit
     }
 }
@@ -20,7 +24,10 @@ if (-Not (Test-Path -Path $binPath)) {
 Write-Host "`n> Downloading binary with Invoke-WebRequest..."
 Invoke-WebRequest -Uri "https://github.com/Olypolyu/Kotlin-Encabulator/releases/download/$latest/kt-encabulator.exe" -OutFile $exePath
 
-Write-Host "`n`e[1;32mReady to go!`e[0m Execute with `"`e[1;34mkt-encabulator.exe`e[0m`""
+Write-Host -NoNewline "`nReady to go! " -ForegroundColor Green
+Write-Host -NoNewline "Execute with "
+Write-Host "`"kt-encabulator.exe`"" -ForegroundColor Blue
+
+Write-Host "  - Add $binPath to your PATH to run from anywhere"
 Write-Host "  - Run with -h or --help for documentation."
-Write-Host "  - Add "$binPath" to your PATH to run from anywhere"
 Write-Host " "
