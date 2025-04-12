6efda4f6219f9a10909f05bd19d2a09483a7b809
Oly <Oly@yamums.house>
2025-04-12 18:46:10 -0300

    fix linux install script

--- script/install-linux.sh
+++ script/install-linux.sh
@@ -2,15 +2,21 @@
 LASTEST="test"
 
 if [ -f "$HOME/.local/bin/kt-encabulator" ]; then
-  read -p "Found existing binary in local bin. Do you wish to Continue (y/n)?" choice
+  echo -e "> Found existing binary in local bin. Do you wish to Continue? \e[1;34m(y/n)\e[0m"
 
+  read choice < /dev/tty
   case "$choice" in
     y|Y ) rm -r "$HOME/.local/bin/kt-encabulator";;
-    * ) echo "Exiting."; exit;;
+    * ) echo "The existing file will not be overwritten. Exiting..."; exit;;
   esac
 fi
 
-echo "Downloading file with curl."
+echo " "
+echo -e "> Downloading binary with curl...\n"
 curl -L -o "$HOME/.local/bin/kt-encabulator" "https://github.com/Olypolyu/Kotlin-Encabulator/releases/download/$LASTEST/kt-encabulator"
 chmod +x "$HOME/.local/bin/kt-encabulator"
-echo "Ready to go! Execute with `$kt-encabulator`"
+
+echo " "
+echo -e "\e[1;32mReady to go!\e[0m Execute with \"\e[1;34mkt-encabulator\e[0m\""
+echo "  - Run with -h or --help for documentation."
+echo " "
